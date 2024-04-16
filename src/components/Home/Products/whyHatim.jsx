f1e574376d806a6f9684e9fd0d89b68a0eb83b62
vaibhav_rathore <vaibahvrathorema@gmail.com>
2024-04-16 13:31:05 +0530

    add cont animation

--- src/components/Home/Products/whyHatim.jsx
+++ src/components/Home/Products/whyHatim.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../../styles/Home/Product/why_Hamis.css";
-
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 function WhyHamis() {
+  const [CountOn, setCountOn] = useState(false);
   return (
+    <ScrollTrigger onEnter={()=>setCountOn(true) } onExit={()=>setCountOn(false)} >
     <div className="">
       {/* heading */}
       <div
-        className="main_heading Why_Hamis_text"
+        className="main_heading Why_Hamis_text" 
         data-aos="zoom-in"
         data-aos-offset="200"
         data-aos-delay="800"
@@ -17,10 +20,10 @@ function WhyHamis() {
       {/* details */}
       <div
         className="why_hamis_details_div"
-        data-aos="zoom-in"
-        data-aos-offset="200"
-        data-aos-delay="800"
-        data-aos-duration="1000"
+        // data-aos="zoom-in"
+        // data-aos-offset="200"
+        // data-aos-delay="800"
+        // data-aos-duration="1000"
       >
         {/* 1 */}
         <div className="why_hamis_details">
@@ -29,16 +32,17 @@ function WhyHamis() {
         </div>
         {/* 2 */}
         <div className="why_hamis_details">
-          <div className="why_hamis_details_num">100+</div>
+          <div className="why_hamis_details_num"> {CountOn && <CountUp  start={0} end={100} duration={5} /> }+ </div>
           <div className="why_hamis_details_texts">Dealers</div>
         </div>
         {/* 3 */}
         <div className="why_hamis_details">
-          <div className="why_hamis_details_num">10+</div>
+          <div className="why_hamis_details_num"> {CountOn &&  <CountUp start={0} end={10} duration={6} />} +</div>
           <div className="why_hamis_details_texts">Serving Countries</div>
         </div>
       </div>
     </div>
+    </ScrollTrigger>
   );
 }
 
